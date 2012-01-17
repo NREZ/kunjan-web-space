cc51f190a194c3eb6da3122dff36dfd2b6de8cc6
Kunjan Dalal <kunjee17@gmail.com>
2012-01-17 12:12:28 +0530

    Started DemoApplication
    This demo application contains demo of all the articles I am doing.

---
+++ DemoApplication/DemoApplication/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DemoApplication")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("DemoApplication")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a53715e6-b00f-4f25-abe1-ffefc17f9c46")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
