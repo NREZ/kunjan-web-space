2b22dcf46621911795cb4eb9b92e1b70421dca0e
Kunjan Dalal <kunjee17@gmail.com>
2012-02-14 11:22:22 +0530

    Demo Application Updated

--- DemoApplication/DemoApplication/Controllers/CustomersController.cs
+++ DemoApplication/DemoApplication/Controllers/CustomersController.cs
@@ -8,96 +8,96 @@ using DemoApplication.Models;
 
 namespace DemoApplication.Controllers
 {   
-    public class CustomersController : Controller
-    {
+	public class CustomersController : Controller
+	{
 		private readonly ICustomerRepository customerRepository;
 
-        public CustomersController(ICustomerRepository customerRepository)
-        {
+		public CustomersController(ICustomerRepository customerRepository)
+		{
 			this.customerRepository = customerRepository;
-        }
+		}
 
-        //
-        // GET: /Customers/
+		//
+		// GET: /Customers/
 
-        public ViewResult Index()
-        {
-            return View(customerRepository.AllIncluding(customer => customer.Orders));
-        }
+		public ViewResult Index()
+		{
+			return View(customerRepository.AllIncluding(customer => customer.Orders));
+		}
 
-        //
-        // GET: /Customers/Details/5
+		//
+		// GET: /Customers/Details/5
 
-        public ViewResult Details(string id)
-        {
-            return View(customerRepository.Find(id));
-        }
+		public ViewResult Details(string id)
+		{
+			return View(customerRepository.Find(id));
+		}
 
-        //
-        // GET: /Customers/Create
+		//
+		// GET: /Customers/Create
 
-        public ActionResult Create()
-        {
-            return View();
-        } 
+		public ActionResult Create()
+		{
+			return View();
+		} 
 
-        //
-        // POST: /Customers/Create
+		//
+		// POST: /Customers/Create
 
-        [HttpPost]
-        public ActionResult Create(Customer customer)
-        {
-            if (ModelState.IsValid) {
-                customerRepository.InsertOrUpdate(customer);
-                customerRepository.Save();
-                return RedirectToAction("Index");
-            } else {
+		[HttpPost]
+		public ActionResult Create(Customer customer)
+		{
+			if (ModelState.IsValid) {
+				customerRepository.InsertOrUpdate(customer);
+				customerRepository.Save();
+				return RedirectToAction("Index");
+			} else {
 				return View();
 			}
-        }
-        
-        //
-        // GET: /Customers/Edit/5
+		}
+		
+		//
+		// GET: /Customers/Edit/5
  
-        public ActionResult Edit(string id)
-        {
-             return View(customerRepository.Find(id));
-        }
+		public ActionResult Edit(string id)
+		{
+			 return View(customerRepository.Find(id));
+		}
 
-        //
-        // POST: /Customers/Edit/5
+		//
+		// POST: /Customers/Edit/5
 
-        [HttpPost]
-        public ActionResult Edit(Customer customer)
-        {
-            if (ModelState.IsValid) {
-                customerRepository.InsertOrUpdate(customer);
-                customerRepository.Save();
-                return RedirectToAction("Index");
-            } else {
+		[HttpPost]
+		public ActionResult Edit(Customer customer)
+		{
+			if (ModelState.IsValid) {
+				customerRepository.InsertOrUpdate(customer);
+				customerRepository.Save();
+				return RedirectToAction("Index");
+			} else {
 				return View();
 			}
-        }
+		}
 
-        //
-        // GET: /Customers/Delete/5
+		//
+		// GET: /Customers/Delete/5
  
-        public ActionResult Delete(string id)
-        {
-            return View(customerRepository.Find(id));
-        }
+		public ActionResult Delete(string id)
+		{
+			return View(customerRepository.Find(id));
+		}
 
-        //
-        // POST: /Customers/Delete/5
+		//
+		// POST: /Customers/Delete/5
 
-        [HttpPost, ActionName("Delete")]
-        public ActionResult DeleteConfirmed(string id)
-        {
-            customerRepository.Delete(id);
-            customerRepository.Save();
+		[HttpPost, ActionName("Delete")]
+		public ActionResult DeleteConfirmed(string id)
+		{
+			customerRepository.Delete(id);
+			customerRepository.Save();
 
-            return RedirectToAction("Index");
-        }
-    }
+			return RedirectToAction("Index");
+		}
+	}
 }
 
